1525da30ffe2d07bb2ae60fccb673a58cf2de272
Curtis Yacboski <curtisyacboski@gmail.com>
2019-07-09 17:26:55 -0700

    added unit testing for redux thunk

--- src/store/integraton.test.js
+++ src/store/integraton.test.js
@@ -2,20 +2,70 @@ import { storeFactory } from '../utils/testingFunctions';
 import { guessWord } from './actions'
 
 describe('guess word action dispatcher', () => {
-    describe('no guess words', () => {
-        it('for unsuccessful guess it updates the state accordingly', () => {
+    const correctWord = "unclebox";
+    const incorrectWord = 'uzzz';
 
+    describe('no guess words', () => {
+        let store;
+        const initState = { secretWord: correctWord };
+
+        beforeEach(() => {
+            store = storeFactory(initState);
+        })
+        it('for unsuccessful guess it updates the state accordingly', () => {
+            store.dispatch(guessWord(incorrectWord));
+            const newState = store.getState();
+            const expectState = {
+                ...initState,
+                success: false,
+                guessedWords: [{
+                    guessedWord: incorrectWord,
+                    letterMatchCount: 1
+                }]
+            }
+            expect(newState).toEqual(expectState);
         })
         it('for successful guess it updates the state accordingly', () => {
-            
+            store.dispatch(guessWord(correctWord));
+            const newState = store.getState();
+            const expectState = {
+                ...initState,
+                success: true,
+                guessedWords: [{
+                    guessedWord: correctWord,
+                    letterMatchCount: 8
+                }]
+            }
+            expect(newState).toEqual(expectState);
         })
     })
     describe('some guess words', () => {
-        it('for unsuccessful guess it updates the state accordingly', () => {
+        const guessedWords = [{ guessedWord: "jacky", letterMatchCount: 3 }]
+        const initState = { guessedWords, secretWord: correctWord };
 
+        let store;
+        beforeEach(() => {
+            store = storeFactory(initState);
+        })
+        it('for unsuccessful guess it updates the state accordingly', () => {
+            store.dispatch(guessWord(incorrectWord));
+            const newState = store.getState();
+            const expectState = {
+                secretWord: correctWord,
+                success: false,
+                guessedWords: [...guessedWords, { guessedWord: incorrectWord, letterMatchCount: 1 }]
+            }
+            expect(newState).toEqual(expectState);
         })
         it('for successful guess it updates the state accordingly', () => {
-            
+            store.dispatch(guessWord(correctWord));
+            const newState = store.getState();
+            const expectState = {
+                secretWord: correctWord,
+                success: true,
+                guessedWords: [...guessedWords, { guessedWord: correctWord, letterMatchCount: 8 }]
+            }
+            expect(newState).toEqual(expectState);
         })
     })
 })
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import success from './success';
 import guessedWords from './guessedWords';
+import secretWord from './secretWord';
 
 export default combineReducers({
     success,
-    guessedWords
+    guessedWords,
+    secretWord
 })
