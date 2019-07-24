da4f08a9d49ed4843086ac7790bea53d494c9626
Curtis Yacboski <curtisyacboski@gmail.com>
2019-07-24 15:09:29 -0700

    added failed events for moxios

--- src/store/actions/index.test.js
+++ src/store/actions/index.test.js
@@ -28,4 +28,23 @@ describe('getSecretWord action creator', () => {
                 expect(newState.secretWord).toBe(secretWord);
             })
     })
+    it('should throw a network error if failed to load secret word', () => {
+        const errorMessage = "Network Error";
+        const store = storeFactory({ networkError: errorMessage });
+        const errorResponse = {
+            status: 400,
+            response: { message: errorMessage }
+        }
+
+        moxios.wait(() => {
+            let req = moxios.requests.mostRecent();
+            req.reject(errorResponse);
+        });
+
+        return store.dispatch(getSecretWord())
+            .catch(() => {
+                const state = store.getState();
+                expect(state.networkError).toBe(errorMessage);
+            })
+    })
 })
